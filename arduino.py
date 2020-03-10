b36b07e9f0cb9d8a318a4e1116a36725cd8fc3a6
JovianHawkMoon <53735049+JovianHawkMoon@users.noreply.github.com>
2020-03-10 22:41:21 +1100

    Update arduino.py

--- arduino.py
+++ arduino.py
@@ -5,13 +5,14 @@ arduino.baudrate = 115200
 while True:
 
     dyn_id = (int(input("Enter id:"))).to_bytes(1, byteorder="little")
-    speed = (int(input("Enter speed:"))).to_bytes(2, byteorder="little")
-    print(dyn_id,speed)
-    b_dyn_id = bytearray(dyn_id)
-    b_speed = bytearray(speed)
-    print(b_dyn_id[0],b_speed[0],b_speed[1])
-    b_dyn_id.append(b_speed[1])
-    b_dyn_id.append(b_speed[0])
-    print(b_dyn_id)
-    print(b_dyn_id[0],b_dyn_id[1],b_dyn_id[2])
-    arduino.write(b_dyn_id)
+    com = (int(input("Enter command type (1 torque) (2 Speed) (3 Postion):"))).to_bytes(1, byteorder="little")
+    val = (int(input("Enter value:"))).to_bytes(2, byteorder="little")
+
+    ba_dyn_id = bytearray(dyn_id)
+    ba_com = bytearray(com)
+    ba_val = bytearray(val)
+    
+    ba_dyn_id.append(ba_com[0])
+    ba_dyn_id.append(ba_val[1])
+    ba_dyn_id.append(ba_val[0])
+    arduino.write(ba_dyn_id)
