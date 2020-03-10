f743696c581521b24792cedc6a8c7c72db42567a
JovianHawkMoon <53735049+JovianHawkMoon@users.noreply.github.com>
2020-03-10 21:47:27 +1100

    Update arduino.py

--- arduino.py
+++ arduino.py
@@ -1,11 +1,17 @@
 import serial
-arduino = serial.Serial("/dev/ttyUSB0")
+arduino = serial.Serial("\\\\.\\COM8")
 arduino.baudrate = 115200
 
 while True:
 
-    dyn_id = (int(input("Enter id:")))
+    dyn_id = (int(input("Enter id:"))).to_bytes(1, byteorder="little")
     speed = (int(input("Enter speed:"))).to_bytes(2, byteorder="little")
-
-    message = bytearray(speed)
-    arduino.write(message)
+    print(dyn_id,speed)
+    b_dyn_id = bytearray(dyn_id)
+    b_speed = bytearray(speed)
+    print(b_dyn_id[0],b_speed[0],b_speed[1])
+    b_dyn_id.append(b_speed[1])
+    b_dyn_id.append(b_speed[0])
+    print(b_dyn_id)
+    print(b_dyn_id[0],b_dyn_id[1],b_dyn_id[2])
+    arduino.write(b_dyn_id)
