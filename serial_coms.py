e38801773c15b3b79db91148a9de2dfb00c3a24e
Charles Raffaele <u3201136@uni.canberra.edu.au>
2020-04-10 17:51:30 +1000

    trying things

--- serial_coms.py
+++ serial_coms.py
@@ -20,18 +20,17 @@ def position(value):
     arduino.write(ba_dyn_id)
 
 
-if __name__ == "main":
-    while True:
+while True:
 
-        dyn_id = (int(input("Enter id:"))).to_bytes(1, byteorder="little")
-        com = (int(input("Enter command type (1 torque) (2 Speed) (3 Postion):"))).to_bytes(1, byteorder="little")
-        val = (int(input("Enter value:"))).to_bytes(2, byteorder="little")
+    dyn_id = (int(input("Enter id:"))).to_bytes(1, byteorder="little")
+    com = (int(input("Enter command type (1 torque) (2 Speed) (3 Postion):"))).to_bytes(1, byteorder="little")
+    val = (int(input("Enter value:"))).to_bytes(2, byteorder="little")
 
-        ba_dyn_id = bytearray(dyn_id)
-        ba_com = bytearray(com)
-        ba_val = bytearray(val)
+    ba_dyn_id = bytearray(dyn_id)
+    ba_com = bytearray(com)
+    ba_val = bytearray(val)
 
-        ba_dyn_id.append(ba_com[0])
-        ba_dyn_id.append(ba_val[1])
-        ba_dyn_id.append(ba_val[0])
-        arduino.write(ba_dyn_id)
+    ba_dyn_id.append(ba_com[0])
+    ba_dyn_id.append(ba_val[1])
+    ba_dyn_id.append(ba_val[0])
+    arduino.write(ba_dyn_id)
